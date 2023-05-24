67402c3889c304887ce4757333299b8ccbcc969b
Matthew Peddle <matthew.t.peddle@gmail.com>
2023-05-23 23:27:41 -0400

    Update func.py

--- func.py
+++ func.py
@@ -7,13 +7,32 @@ from fdk import response
 
 nvidia_url = "https://objectstorage.us-ashburn-1.oraclecloud.com/p/xik5PN53LN92vcDgAlQAjaWwajuJdEJSb7jxlNFCWCFcSnr2DFTrSv6xZgOTKIYP/n/hpc_limited_availability/b/Nvidia_Handover/o/"
 
+funcDefinition = {
+    "status": {
+        "returnCode": 0,
+        "errorMessage": ""
+    },
+    "funcDescription": {
+        "outputs": [],
+        "parameters": [],
+        "bucketName": "bucket-OCI-FAAS",
+        "isOutputJoinableWithInput": True
+    }
+}
+
+
 def handler(ctx, data: io.BytesIO=None):
-    # name = "World"
-    # try:
-    #     body = json.loads(data.getvalue())
-    #     name = body.get("name")
-    # except (Exception, ValueError) as ex:
-    #     logging.getLogger().info('error parsing json payload: ' + str(ex))
+    response_data = ""
+    # DESCRIBE FUNCTION
+    try:
+        body = json.loads(data.getvalue())
+        funcMode = body.get("funcMode")
+        if funcMode == 'describeFunction':
+           response_data = json.dumps(funcDefinition)
+    except (Exception, ValueError) as ex:
+        response_data = json.dumps(
+            {"error": "{0}".format(str(ex))})
+    # EXECUTE FUNCTION
     resp = None
     try:
         logging.getLogger().debug('payload: ' + str(data))
@@ -23,6 +42,6 @@ def handler(ctx, data: io.BytesIO=None):
 
     return response.Response(
         ctx, response_data=json.dumps(
-            {"message": "Status {0}".format(resp.status_code)}),
+            {"message": "Status {0}".format(resp)}),
         headers={"Content-Type": "application/json"}
     )
